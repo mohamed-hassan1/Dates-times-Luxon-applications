4989514d2918c898392b76bae262c14fd73bb149
mohamed-hassan1 <mohamedhassanmohamed1@gmail.com>
2025-01-17 18:13:49 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// *** First Application ***
+/*
+assumbtions:
+- csv file contains data for persons
+- birth date in the csv file formated as MM/DD/YYYY
+- birth time formated as 12 hour (am, pm)
+
+Task requirements:
+- change the birth date format to matach the person country format (as posible)
+- calculate the age of the person in 1st October 2025
+- add the calculated age to the person data
+- create a paragraph from the formated data
+- save new data in a new file
+
+*/
+
+// you will not copy the code .. you will write it yourself. character by character.
